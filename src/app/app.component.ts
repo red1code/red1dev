69893b09694f7bfbe7531e0d9240fb4a16c80c07
Redouane Bekkouche <redouane.bekk@gmail.com>
2024-01-08 08:07:31 +0100

    add scroll to top & telegram buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -12,9 +13,30 @@ import { FooterComponent } from './components/footer/footer.component';
     RouterOutlet,
     HeaderComponent,
     FooterComponent,
+    NgOptimizedImage,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+
+  showGoToTopBtn = false;
+
+  constructor() { }
+
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+    if (window.scrollY > window.innerHeight) {
+      this.showGoToTopBtn = true;
+    }
+    else {
+      this.showGoToTopBtn = false;
+    }
+  }
+
+  scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+
 }

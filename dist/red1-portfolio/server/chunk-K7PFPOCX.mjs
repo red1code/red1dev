c3273342ab78093423c96957c5cef21d8a7aa77f
Redouane Bekkouche <redouane.bekk@gmail.com>
2024-01-10 17:55:37 +0100

    setup things for github pages (SSG)

---
+++ dist/red1-portfolio/server/chunk-K7PFPOCX.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{v as n}from"./chunk-43YJ2NK5.mjs";import{$ as o,U as i}from"./chunk-R6CTAQZ3.mjs";var E=(()=>{let t=class t{};t.\u0275fac=function(r){return new(r||t)},t.\u0275mod=o({type:t}),t.\u0275inj=i({imports:[n,n]});let e=t;return e})();export{E as a};

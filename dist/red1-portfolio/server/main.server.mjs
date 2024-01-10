933ba8321fdd71ae8f0802234d2cffa03e9396ef
Redouane Bekkouche <redouane.bekk@gmail.com>
2024-01-10 18:23:41 +0100

    delete dist folder

--- dist/red1-portfolio/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-MCQ5IRS7.mjs";import"./chunk-4KXYVJVW.mjs";import"./chunk-K7PFPOCX.mjs";import"./chunk-43YJ2NK5.mjs";import"./chunk-R6CTAQZ3.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
